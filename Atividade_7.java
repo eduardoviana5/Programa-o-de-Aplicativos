d6afb27c5a64560c5434bceddc6c80f9527cefdf
eduardoviana5 <dudumav05@gmail.com>
2026-02-11 16:55:56 -0300

    Add files via upload

--- Atividade_7.java
+++ Atividade_7.java
@@ -5,6 +5,15 @@ public class Atividade_7 {
 
         Scanner sc = new Scanner(System.in);
 
-        double produto;
+        float produto;
+        int qtd;
+
+        System.out.printf("Informe o valor do produto: ");
+        produto = sc.nextFloat();
+
+        System.out.printf("Informe a quantidade de produto comprado: ");
+        qtd = sc.nextInt();
+
+        System.out.printf("Valor total da compra: %.2f ", produto * qtd);
     }
 }
