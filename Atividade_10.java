785b7d9f805f1d3c2cae5f433ce84d6b6d2d3f0c
eduardoviana5 <dudumav05@gmail.com>
2026-02-13 19:45:43 -0300

    Create Atividade_10.java

---
+++ Atividade_10.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Atividade_10 {
+    
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        boolean valor = true;
+
+        System.out.printf("\nO valor da variavel boolean é %b", valor);
+
+        sc.close();
+    } 
+}
