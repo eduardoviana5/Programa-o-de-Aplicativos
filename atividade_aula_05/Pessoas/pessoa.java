71202044963127e6c3f43374e8c59a6b6056dd13
Eduardo Meneses de Almeida Viana <dudumav05@gmail.com>
2026-03-14 01:41:57 -0300

    Update pessoa.java

--- atividade_aula_05/Pessoas/pessoa.java
+++ atividade_aula_05/Pessoas/pessoa.java
@@ -1,19 +1,31 @@
-public class pessoa{
+public class Pessoa{
 
     private String nome;
     private int idade;
 
-
-    //construtor
-    public pessoa(String nome, int idade){
+    public Pessoa(String nome, int idade){
         this.nome = nome;
         this.idade = idade;
     }
 
-    //metodos
-    public void exibirDados(){
-        System.out.println("Nome: "+ this.nome);
-        System.out.println("Idade: "+ this.idade + " anos");
+    public void setNome(String nome){
+        this.nome = nome;
     }
 
+    public String getNome(){
+        return this.nome;
+    }
+
+    public void setIdade(int idade){
+        this.idade = idade;
+    }
+
+    public int getIdade(){
+            return this.idade;
+    }
+
+    public void exibirDados(){
+        System.out.println("Nome: " + this.nome);
+        System.out.println("Idade: " + this.idade + " anos");
+    }
 }
